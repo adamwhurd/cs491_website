7cdc3ee11c310bb5caef3e728c8f7842ae891af1
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2023-04-21 15:30:52 -0700

    ww

--- lib/cs491_website_web/controllers/employee_api_json.ex
+++ lib/cs491_website_web/controllers/employee_api_json.ex
@@ -2,7 +2,7 @@ defmodule Cs491WebsiteWeb.EmployeeApiJSON do
   def index(%{employees: employees}) do
     %{data:
       for (employee <- employees) do
-        [%{last_name: employee.last_name}]
+        %{last_name: employee.last_name}
 
       end
     }
